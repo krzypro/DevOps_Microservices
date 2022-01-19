3b1ef92bf156114fa39cbda82a1cd2248379a67c
dev <dev@think.good>
2022-01-20 00:57:40 +0100

    Task 2 - run container and make a prediction

--- project-ml-microservice-kubernetes/run_docker.ps1
+++ project-ml-microservice-kubernetes/run_docker.ps1
@@ -1,9 +1,3 @@
-#!/usr/bin/env bash
-
-## Complete the following steps to get Docker running locally
-
-# Step 1:
-# Build image and add a descriptive tag
 docker build --tag=projecttag .
 
 # Step 2: 
