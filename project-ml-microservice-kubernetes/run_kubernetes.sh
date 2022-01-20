e614e48b98ae8394f0f8f2c368383ff5bf599c4e
dev <dev@think.good>
2022-01-20 01:31:41 +0100

    Task 6 - deploy with Kubernates and save logs

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="pkrzysztoff/projecttag"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacity\
+    --image=$dockerpath\
+    --port=80 --labels app=udacity
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacity 8000:80
